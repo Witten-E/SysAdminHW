8b093a1921327eea3dfb7f8d160c2293d93cede1
Eva Witten <616coolaj@gmail.com>
2025-09-18 17:24:05 -0400

    init commit

---
+++ ping_test.py
@@ -0,0 +1,16 @@
+
+
+def print_options():
+    print('''
+    1. Display the default gateway
+    2. Test local connectivity
+    3. Test remote connectivity
+    4. Test DNS resolution
+    5. Exit/quit the script''')
+    
+    
+def main():
+    print_options()
+
+if __name__ == "__main__":
+    main()
